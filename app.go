eb10ca48a208b0a3938fb6a248fecc5ffee6526b
pyncz <pavelyanycz@gmail.com>
2022-08-18 00:19:16 +0400

    feat: Enable middlewares

--- app.go
+++ app.go
@@ -7,6 +7,9 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 
 	api_v1 "pyncz/go-rest/api/v1"
@@ -42,7 +45,10 @@ func main() {
 	app.Static("/", "./public")
 	app.Mount("/api/v1", api_v1.App(env))
 
+	app.Use(favicon.New())
 	app.Use(middlewares.NotFound)
+	app.Use(limiter.New())
+	app.Use(recover.New())
 
 	// Start server on provided port
 	port := os.Getenv("PORT")
@@ -50,7 +56,7 @@ func main() {
 		port = "9090"
 	}
 
-	log.Fatal(
+	logger.Fatal(
 		app.Listen(fmt.Sprintf(":%s", port)),
 	)
 }
